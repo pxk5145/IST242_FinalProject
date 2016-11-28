70a117a354c5dbc2c72446d05b04379fa62faf65
Shawn <Shawn@Shawnslaptop>
2016-11-28 14:47:55 -0500

    changed the template image

--- src/NavViewSplashPanel.java
+++ src/NavViewSplashPanel.java
@@ -6,7 +6,7 @@ public class NavViewSplashPanel extends JPanel{
     JLabel optionsLabel;
     NavViewSplashPanel()
     {
-            splashButton = new JButton(new ImageIcon("src/images/codeattacks.png"));            
+            splashButton = new JButton(new ImageIcon("src/images/game_template.png"));            
             add(splashButton);
             
             optionsLabel = new JLabel("No options selected yet.");
